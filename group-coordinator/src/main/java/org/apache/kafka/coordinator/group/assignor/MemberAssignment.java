56ef23c445c0e5b9938720384d00134a04082af1
Ritika Reddy <rreddy@confluent.io>
2023-04-13 08:29:38 -0700

    Removed Topic Name, minor changes

--- group-coordinator/src/main/java/org/apache/kafka/coordinator/group/assignor/MemberAssignment.java
+++ group-coordinator/src/main/java/org/apache/kafka/coordinator/group/assignor/MemberAssignment.java
@@ -58,6 +58,6 @@ public class MemberAssignment {
 
     @Override
     public String toString() {
-        return "MemberAssignment (Target partitions = " + targetPartitions + ')';
+        return "MemberAssignment(targetPartitions=" + targetPartitions + ')';
     }
 }
